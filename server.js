32a9b3fddfe7915015e054d317c1f84737b4ca2a
philha <philip.hanson@calrec.com>
2017-05-30 15:29:42 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//templating
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//Middleware
+//Remember middleware is called in the order that you call app.use
+app.use((request, response, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${request.method} ${request.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log',log +'\n', (err) => {
+		if(err) {
+			console.log('unable to append to server.log');
+		}
+	});
+	next();
+});
+
+//Will pause execution because we dont call next
+// app.use((request, response, next) => {
+// 	response.render('maintenance.hbs', {
+// 		pageTitle: 'Maintenance',
+// 	});
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+
+//handlebar helpers
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+
+app.get('/', (request, response) =>{
+	response.render('home.hbs', {
+		welcomeMessage: 'Welcome to the site',
+		pageTitle: 'Home'
+	});
+});
+
+app.get('/about', (request, response) => {
+	response.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/bad', (request, response) => {
+	response.send({
+		errorMessage: 'Bad URL'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});

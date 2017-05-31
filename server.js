977c359868ef4e1eca3947f224d34debfcfb8d8a
philha <philip.hanson@calrec.com>
2017-05-31 11:13:03 +0100

    setup start script and Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 //templating
@@ -62,6 +64,9 @@ app.get('/bad', (request, response) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Server is up on port 3000');
+//use 300 locally
+//on Heroku
+//use herokus Environment variable
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
 });
